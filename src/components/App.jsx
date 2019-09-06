97d8d18a3efd77618b28aa02d3a9cd531e67e5fe
Jessica Doss <doss.jessicam@gmail.com>
2019-09-06 08:54:39 -0700

    continue project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+    <h1>Lucy</h1>
+    <h3>A tap room in NE</h3>
+    <hr/>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,26 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/*
-  issue with react-hot-loader
-  eventhough those 2 dependencies are only used in development
-  eslint has no way to tell that and outputs an error
-*/
-
-// react dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-// hot reload for development
+import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
-
-import App from './App';
-
-import './style.scss';
-
-const root = document.getElementById('root');
+import { HashRouter } from 'react-router-dom';
+// import App from './App';
+//
+// import './style.scss';
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
-    </AppContainer>,
-    root,
+      <HashRouter>
+      <Component/>
+      </HashRouter>
+      </AppContainer>,
+    document.getElementById('react-app-root')
   );
 };
 
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./App', () => { render(App); });
+  module.hot.accept('./components/App', () => { render(App);
+  });
 }
