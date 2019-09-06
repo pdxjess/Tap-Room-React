9f96bb057dd7a77b0db011f867df709ebcb470e5
Jessica Doss <doss.jessicam@gmail.com>
2019-09-06 09:05:08 -0700

    add route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import BeerList from './BeerList';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -6,6 +8,9 @@ function App() {
     <h1>Lucy</h1>
     <h3>A tap room in NE</h3>
     <hr/>
+      <Switch>
+        <Route exact path='/' component={BeerList} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/BeerList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function BeerList() {
+  return (
+    <div>
+      <h3>Beer name</h3>
+      <ul>
+        <li>brewer</li>
+        <li>description</li>
+        <li>price</li>
+        <li>remaining</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BeerList;
