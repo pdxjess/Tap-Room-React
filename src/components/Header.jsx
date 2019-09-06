e7ef73b88e9cac7275d0494cc473d2d00d1607a9
Jessica Doss <doss.jessicam@gmail.com>
2019-09-06 10:24:49 -0700

    add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
-    <h1>Lucy</h1>
+    <div className="container">
+      <div className="header-top">
+        <h1>Lucy</h1>
+        <ul className="nav-bar">
+          <li><Link to="/Header">Beers</Link></li>
+          <li><Link to="/Header">About</Link></li>
+          <li><Link to="/Header">Location</Link></li>
+          <li><Link to="/Header">Sign up</Link></li>
+          <li><Link to="/Header">Log in</Link></li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
