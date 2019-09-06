febe6fb674323e4671494eb060fd97b2640b1c96
Jessica Doss <doss.jessicam@gmail.com>
2019-09-06 15:05:12 -0700

    still working through routing errors

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from './App.jsx';
+import Tagline from './Tagline';
+
+
+function Header(){
+  return (
+    <div className="container">
+      <div className="header-top">
+        <h1>Lucy</h1>
+        <Tagline />
+        <Router>
+        <ul className="nav-bar">
+          <li><Link to="./Beer">Beers</Link></li>
+          <li><Link to="./Header">About</Link></li>
+          <li><Link to="./Header">Location</Link></li>
+          <li><Link to="./Header">Sign up</Link></li>
+          <li><Link to="./Header">Log in</Link></li>
+        </ul>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
